cc8cb0d7ca9f192e9ed06f4348a348eceef6d974
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-08-27 14:09:39 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,11 +1,37 @@
 const expect = require('chai').expect;
 
 function vitalsAreOk(bpm, spo2, respRate) {
-    if (!(70 <= bpm <= 150) || (spo2<90) ||  !(30 <= respRate <=95){
-        return false;
-    } 
+  
+    if(BPMinRange(bpm))
+    {return false;
+    }
+    else if(SPO2inRange(spo2))
+    {return false;
+    }
+    else if(resprateinRange(respRate))
+    {return false;
+    }
     return true;
 }
+        
+function BPMinRange(bpm){
+        if(!(70 <= bpm <= 150))
+            return false;
+        return true;
+    }
+function SPO2inRange(spo2){
+    if(spo2<90)
+        return false;
+    return true;
+}
+function respRateinRange(respRate){
+    if(!(30 <= respRate <=95))
+        return false;
+    return true;
+}
+    
+    
+        
 
 expect(vitalsAreOk(100, 95, 70)).to.be.true;
 expect(vitalsAreOk(50, 95, 70)).to.be.false;
