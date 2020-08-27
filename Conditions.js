07699f70730c8119ddba51284feee2263d9e9df6
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-08-27 14:25:33 +0530

    Update Conditions.js

--- Conditions.js
+++ Conditions.js
@@ -1,18 +1,18 @@
 function BPMinRange(bpm){
         if(!(70 <= bpm <= 150))
-            return false;
+        {return false;}
         return true;
     }
     
 function SPO2inRange(spo2){
     if(spo2<90)
-        return false;
+    {return false;}
     return true;
 }
 
 function respRateinRange(respRate){
     if(!(30 <= respRate <=95))
-        return false;
+    {return false;}
     return true;
 }
 
@@ -20,5 +20,5 @@ module.exports={
 BPMinRange:BPMinRange;
 SPO2inRange:SPO2inRange;
 respRateinRange:respRateinRange;
-}
+};
 
